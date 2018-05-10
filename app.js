b53d13d96d80f3da4625adf02cc2e510739ca2d6
catfrance <38217286+catfrance@users.noreply.github.com>
2018-05-10 21:15:52 +0100

    final

--- app.js
+++ app.js
@@ -1,42 +1,73 @@
-﻿// static posts Vue component
-Vue.component('static-posts', {
+﻿// Base Url of the API
+const baseUrl = "http://jsonplaceholder.typicode.com/";
 
-    // setting the template that this component will use to render
-    template: '#static-posts-template',
-
-    // the data function is where we define all the variables this component will need
-    // in this specific case, we only need to worry about an array of posts
+// List component
+const List = {
+    template: '#list-template',
     data: () => ({
-        posts: []
+        posts: [],
+        search: ""
     }),
-
-    // this is called whenever this component is mounted onto the DOM
-    // basically whenever we want to show all the posts, we go and get them
     mounted() {
         this.getPosts();
     },
-
-    // this is where you define all the methods this component needs
     methods: {
-
-        // getPost simply sets the 'posts' variable with static data
         getPosts() {
-            this.posts = [
-                {
-                    "title": "The first post title!"
-                },
-                {
-                    "title": "The second post title!"
-                },
-                {
-                    "title": "The third post title!"
-                }
-            ];
+            axios.get(baseUrl + `posts`).then(response => {
+                this.posts = response.data
+                console.log(this.posts);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    },
+    computed: {
+        filteredPosts() {
+            return this.posts.filter(post => {
+                return post.title.includes(this.search);
+            })
         }
     }
+};
+
+// Post component
+const Post = {
+    template: '#post-template',
+    data: () => ({
+        post: null
+    }),
+    mounted() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            var id = this.$route.params.id;
+            axios.get(baseUrl + `posts/` + id).then(response => {
+                this.post = response.data
+                console.log(this.post);
+            }).catch(error => {
+                console.log(error);
+            })
+        }
+    }
+};
+
+// Create vue router
+var router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            name: 'homepage',
+            path: '/',
+            component: List
+        }, {
+            name: 'post',
+            path: '/:id',
+            component: Post
+        }
+    ]
 });
 
-// Create new Vue instance and mount onto elmement with id app
-new Vue({
-    el: '#app'
-});
+// Create vue instance and mount onto #app
+var vue = new Vue({ router });
+var app = vue.$mount('#app');
